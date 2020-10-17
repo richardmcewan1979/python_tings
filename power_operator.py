6e1decf1cae7237d9dee00237a55f30d49d17a87
richardmcewan1979 <richmcewan@hotmail.com>
2020-10-17 17:08:09 +0100

    Add files via upload

---
+++ power_operator.py
@@ -0,0 +1,27 @@
+from __future__ import division
+
+#default python 2.7 
+#power operator 
+
+#yields 0 in python2.7
+print(1/3)
+
+#yields 0.0 in python 2.7
+print(float(1/3))
+
+#0
+print(1//3)
+
+#yields 1 as third
+print(8**(1/3))
+
+print(16**(1/3))
+
+'''
+Ouput in Python 3 etc
+0.333333333333
+0.333333333333
+0
+2.0
+2.51984209979
+'''
